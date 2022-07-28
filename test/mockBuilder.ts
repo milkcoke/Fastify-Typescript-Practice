784dbf949529859e237093ce0b19d8c0870a3328
milkcoke <mbh023@gmail.com>
2022-07-28 20:51:37 +0900

    feat: Add mock server builder
    This is used to jest unit testing.

--- test/mockBuilder.ts
+++ test/mockBuilder.ts
@@ -1,23 +1,21 @@
 import fastify, {FastifyInstance} from "fastify";
-import {healthCheck, heathCheckRoute} from "@test/healthChecker";
-// import fastifyPlugin from 'fastify-plugin';
-// import tap from 'tap';
-// export type Test = typeof tap['Test']['prototype'];
+import {heathCheckRoute} from "@test/healthChecker";
+import fastifyTypeorm from "fastify-typeorm-plugin";
+import { dataSource } from "@db/typeORMDataSource";
+import {usersRoute} from "@controllers/users/user";
 
 // Automatically build and tear down own instance.
-export function build(opts = {}) : FastifyInstance {
+export function build() : FastifyInstance {
 
     const mockServer = fastify({
         logger: false
     });
     mockServer.register(heathCheckRoute);
+    //@ts-ignore
+    mockServer.register(fastifyTypeorm, {connection: dataSource})
     // Ready called when all plugins have been loaded
     // mockServer.ready();
-
-    mockServer.get('/books', async (req, rep)=>{
-        return {hello: 'world'}
-    });
-
+    mockServer.register(usersRoute);
 
 
     return mockServer;
