ee3c475dcdfbf6f26e2fcd101d055f7edecd6902
milkcoke <mbh023@gmail.com>
2022-07-28 20:50:39 +0900

    test: Update jest.config.js configuration
    Set diagnostics : false for keep from type assertion.

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,15 +2,26 @@ const {pathsToModuleNameMapper} = require("ts-jest");
 const {compilerOptions} = require('../tsconfig.json');
 
 module.exports = {
+    testEnvironment: "node", // default: jsdom
     preset: "ts-jest",
     // https://github.com/facebook/jest/issues/3613
     // relative path of project root directory from this config js file
+    globals: {
+        'ts-jest': {
+            diagnostics: false
+        }
+    },
+
     rootDir: '..',
     testMatch: [
         "**/?(*)+(test).ts"
     ],
-
-    testEnvironment: "node", // default: jsdom
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    moduleFileExtensions: [
+        'ts', 'js', 'json'
+    ],
     resetMocks: true,
     clearMocks: true,
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
