93d11b0e3a6bb3fd39d2c64986400eb9e586e21d
milkcoke <mbh023@gmail.com>
2022-08-02 19:48:14 +0900

    feat: Update user entity

--- db/entities/User.ts
+++ db/entities/User.ts
@@ -1,15 +1,15 @@
-import {BaseEntity, Column, Entity, Index, OneToMany} from "typeorm";
+import {BaseEntity, Column, Entity, Index, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import { Contents } from "./Contents";
 
-@Index("user_pk", ["id"], { unique: true })
+@Index("User_pkey", ["id", "loginId"], { unique: true })
 @Entity("User", { schema: "public" })
 export class User extends BaseEntity {
-    @Column("character varying", { primary: true, name: "id", length: 10 })
-    id!: string;
+    @PrimaryGeneratedColumn({ type: "integer", name: "id" })
+    id!: number;
 
     @Column("character varying", { name: "name", nullable: true, length: 20 })
     name!: string | null;
 
-    @OneToMany(() => Contents, (contents) => contents.user)
-    contents!: Contents[];
+    @Column("character varying", { primary: true, name: "login_id", length: 31 })
+    loginId!: string;
 }
